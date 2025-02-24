6ac29b450ed3e26219859da10ef1563808fd8959
Jittrapol Intarasirisawat <jittrapol@gmail.com>
2025-02-24 11:15:05 +0700

    addClass removeClass

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){ //wait until all the elements are loaded
     $("li").click(function(){
         $(this).hide(); //hide the list item I click
     });
+    $("#menu-icon").click(function(){
+        if ($("header nav").hasClass("active")){
+            $("header nav").removeClass("active");
+        }
+        else{
+            $("header nav").addClass("active");
+        }        
+    });
 });
